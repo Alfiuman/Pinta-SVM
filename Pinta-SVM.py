40f89f42891c23f1ac8c2777c7d4c3e173785096
Alfiuman <alfiuman@yahoo.it>
2020-09-15 21:10:06 +0200

    Update Pinta-SVM.py

--- Pinta-SVM.py
+++ Pinta-SVM.py
@@ -8,89 +8,89 @@ from sklearn import svm
 from sklearn.externals import joblib
 
 
-class Svm:
+class SVM:
     
     #Construtor.
-    def __init__(self, file = 0, c = 1.0, algo = 0, testPercSize = 0.3, seed = 0, pickled = 0):
+    def __init__(self, file = 0, c = 1.0, algo = 0, test_perc_size = 0.3, seed = 0, pickled = 0):
         
         if pickled != 0:
-            self.supVecMach = joblib.load(pickled) 
+            self.svm = joblib.load(pickled) 
         else:
             if algo == 0:
-                self.supVecMach = svm.SVC(c)  #rbf
+                self.svm = svm.SVC(c)  #rbf
             elif algo == 1:
-                self.supVecMach = svm.SVC(c, kernel = 'poly')
+                self.svm = svm.SVC(c, kernel = 'poly')
             elif algo == 2:
-                self.supVecMach = svm.SVC(c, kernel = 'linear')
+                self.svm = svm.SVC(c, kernel = 'linear')
             elif algo == 3:
                 if c >= 1.0 or c <= 0.0:
                     c = 0.5
-                self.supVecMach = svm.NuSVC(c)  #rbf
+                self.svm = svm.NuSVC(c)  #rbf
             elif algo == 4:
                 if c >= 1.0 or c <= 0.0:
                     c = 0.5
-                self.supVecMach = svm.NuSVC(c, kernel = 'poly')
+                self.svm = svm.NuSVC(c, kernel = 'poly')
             elif algo == 5:
                 if c >= 1.0 or c <= 0.0:
                     c = 0.5
-                self.supVecMach = svm.NuSVC(c, kernel = 'linear')
+                self.svm = svm.NuSVC(c, kernel = 'linear')
             else:
-                self.supVecMach = svm.LinearSVC(C = c)
+                self.svm = svm.LinearSVC(C = c)
                 
             if file == 0:
                 print("You need to specify a file with data.")
         
         #Prepare the data.
         if file != 0:
-            self.trainAndTest = 1
-            self.testPercSize = testPercSize
-            self.dataFrame = pd.read_csv(file)
-            self.dataFrame.replace('?',-999999, inplace = True)
-            self.dataFrame.drop(self.dataFrame.columns[[0]], 1, inplace = True)
-            self.dataComplete = self.dataFrame.astype(float).values.tolist()
+            self.train_and_test = 1
+            self.test_perc_size = test_perc_size
+            self.dataframe = pd.read_csv(file)
+            self.dataframe.replace('?',-999999, inplace = True)
+            self.dataframe.drop(self.dataframe.columns[[0]], 1, inplace = True)
+            self.data_complete = self.dataframe.astype(float).values.tolist()
             
             random.seed(seed)
-            random.shuffle(self.dataComplete)
-            self.trainData = self.dataComplete[:int((1 - self.testPercSize) * len(self.dataComplete))]
-            self.testData = self.dataComplete[int((1 - self.testPercSize) * len(self.dataComplete)):]
+            random.shuffle(self.data_complete)
+            self.train_data = self.data_complete[:int((1 - self.test_perc_size) * len(self.data_complete))]
+            self.test_data = self.data_complete[int((1 - self.test_perc_size) * len(self.data_complete)):]
             
-            self.trainDataY = list(itertools.chain.from_iterable([sublist[slice(len(self.trainData[0]) - 1,len(self.trainData[0]))] for sublist in self.trainData]))
-            self.trainDataX = [sublist[slice(0,len(self.trainData[0]) - 1)] for sublist in self.trainData]
+            self.train_data_y = list(itertools.chain.from_iterable([sublist[slice(len(self.train_data[0]) - 1,len(self.train_data[0]))] for sublist in self.train_data]))
+            self.train_data_x = [sublist[slice(0,len(self.train_data[0]) - 1)] for sublist in self.train_data]
             
-            self.testDataY = list(itertools.chain.from_iterable([sublist[slice(len(self.testData[0]) - 1,len(self.testData[0]))] for sublist in self.testData]))
-            self.testDataX = [sublist[slice(0,len(self.testData[0]) - 1)] for sublist in self.testData]
+            self.test_data_y = list(itertools.chain.from_iterable([sublist[slice(len(self.test_data[0]) - 1,len(self.test_data[0]))] for sublist in self.test_data]))
+            self.test_data_x = [sublist[slice(0,len(self.test_data[0]) - 1)] for sublist in self.test_data]
             
-            self.yValues = list(set(self.testDataY))
+            self.y_values = list(set(self.test_data_y))
         else:
-            self.trainAndTest = 0
+            self.train_and_test = 0
 
     #Training the model.
     def training(self):
         
-        if self.trainAndTest == 1:
-            return self.supVecMach.fit(self.trainDataX, self.trainDataY)
+        if self.train_and_test == 1:
+            return self.svm.fit(self.train_data_x, self.train_data_y)
         else:
             print("You haven't specified data for training.")
-            return self.supVecMach
+            return self.svm
     
     #Testing the data.
     def testing(self):
         
-        if self.trainAndTest == 1:
-            classifTest = self.supVecMach.predict(self.testDataX)
-            testCheck = [None]*len(classifTest)
+        if self.train_and_test == 1:
+            classif_test = self.svm.predict(self.test_data_x)
+            test_check = [None]*len(classif_test)
             corr = 0
             
-            for i in range(len(classifTest)):
-                if classifTest[i] == self.testDataY[i]:
-                    testCheck[i] = 1
+            for i in range(len(classif_test)):
+                if classif_test[i] == self.test_data_y[i]:
+                    test_check[i] = 1
                     corr += 1
                 else:
-                    testCheck[i] = 0
+                    test_check[i] = 0
             
-            percCorr = corr / len(classifTest)
+            perc_corr = corr / len(classif_test)
             
-            return classifTest, testCheck, percCorr
+            return classif_test, test_check, perc_corr
         else:
             print("You haven't specified data for testing.")
             return np.ndarray([0]), [0], 0
@@ -98,38 +98,38 @@ class Svm:
     #Classifying data.
     def classify(self, classiFile):
         
-        self.dataFrame2 = pd.read_csv(classiFile)
-        self.dataFrame2.replace('?',-99999, inplace = True)
-        self.dataFrame3 = (copy.deepcopy(self.dataFrame2)).astype(float).values.tolist()
-        self.dataFrame2.drop(self.dataFrame2.columns[[0]], 1, inplace = True)
-        self.dataToClass = self.dataFrame2.astype(float).values.tolist()
+        self.dataframe2 = pd.read_csv(classiFile)
+        self.dataframe2.replace('?',-99999, inplace = True)
+        self.dataframe3 = (copy.deepcopy(self.dataframe2)).astype(float).values.tolist()
+        self.dataframe2.drop(self.dataframe2.columns[[0]], 1, inplace = True)
+        self.data_to_class = self.dataframe2.astype(float).values.tolist()
         
-        return self.supVecMach.predict(self.dataToClass)
+        return self.svm.predict(self.data_to_class)
 
     #Exporting the model using pickle.
-    def exportModel(self, nameFile = "svcTrained.pkl"):
+    def export_model(self, nameFile = "svcTrained.pkl"):
         
-        joblib.dump(self.supVecMach, nameFile) 
+        joblib.dump(self.svm, nameFile) 
         
 
 ####################################################################################
 
-a = Svm("breast-cancer-wisconsin.data.txt", algo = 0, seed = 0)  
+a = SVM("breast-cancer-wisconsin.data.txt", algo = 0, seed = 0)  
 #a = Svm(algo = 0, pickled = "svcTrained.pkl")  
 
 b = a.__dict__
 
 a.training()
 
-testResults = a.testing()
+test_results = a.testing()
 
-print(testResults[0])
-print(testResults[1])
-print("Correct percentage: ", testResults[2] * 100, "%", sep = "")
+print(test_results[0])
+print(test_results[1])
+print("Correct percentage: ", test_results[2] * 100, "%", sep = "")
 
 print(a.classify("ToClassProva.txt"))
 
-a.exportModel()
+a.export_model()
 
 
 
